5d31bdf5c0d2d1f6076a850648aa4c4d9864b143
Dario B <feddar@users.noreply.github.com>
2019-12-24 15:15:35 +0100

    Update entrypoint.sh

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-mkdir -p /var/log/exim4/ /etc/letsencrypt/live/noservername.domain.tld/
+FQDN_MAIL=${FQDN_MAIL:-noservername.domain.tld}
+CERT_DIR="/etc/letsencrypt/live/${FQDN_MAIL}"
+LOGDIR="/var/log/exim4"
+CERTDIR=
+
+[ ! -d ${LOGDIR} ] && mkdir -p ${LOGDIR}
+[ ! -d ${CERT_DIR} ] && mkdir -p ${CERT_DIR}
+[ ! -f ${CERT_DIR}/privkey.pem ] && /gencert.sh "${CERT_DIR}"
 
 update-exim4.conf
 
-exec tail -F /var/log/exim4/mainlog &
+exec tail -F ${LOGDIR}/mainlog &
 exec "$@"
