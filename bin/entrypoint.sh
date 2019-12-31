23e630a80ea30840a00daf6794ff507269663b17
Dario B <feddar@users.noreply.github.com>
2019-12-31 18:22:02 +0100

    Update entrypoint.sh

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -4,11 +4,27 @@ FQDN_MAIL=${FQDN_MAIL:-noservername.domain.tld}
 CERT_DIR="/etc/letsencrypt/live/${FQDN_MAIL}"
 LOGDIR="/var/log/exim4"
 
-[ ! -d ${LOGDIR} ] && mkdir -p ${LOGDIR}
+if [ ! -d "${LOGDIR}" ]; then
+    mkdir -p "${LOGDIR}"
+    chown Debian-exim:adm "${LOGDIR}"
+    chmod 750 "${LOGDIR}"
+    chmod g+s "${LOGDIR}"
+fi
+
+if [ ! -f "${LOGDIR}/mainlog" ]; then
+    touch "${LOGDIR}/mainlog"
+    chown Debian-exim:adm "${LOGDIR}/mainlog"
+    chmod 640 "${LOGDIR}/mainlog"
+fi
+
 [ ! -d ${CERT_DIR} ] && mkdir -p ${CERT_DIR}
 [ ! -f ${CERT_DIR}/privkey.pem ] && /gencert.sh 
 
+if [ ! -d /proc/sys/net/ipv6 ]; then 
+    echo 'disable_ipv6 = true' > /etc/exim4/conf.d/main/01_custom
+fi
+
 update-exim4.conf
 
-exec tail -F ${LOGDIR}/mainlog &
+exec tail -f ${LOGDIR}/mainlog &
 exec "$@"
